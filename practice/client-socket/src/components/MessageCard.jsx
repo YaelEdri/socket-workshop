948ca7afc8600cfabec6559369cffe5278f0490c
cannaAriel <cannaariel@carmel6000.amitnet.org>
2021-06-10 11:41:21 +0300

    env

--- practice/client-socket/src/components/MessageCard.jsx
+++ practice/client-socket/src/components/MessageCard.jsx
@@ -5,7 +5,7 @@ const MessageCard = ({ isMe, ip, content, sender, date }) => {
     return (
         <div className={'message-card-container' + (isMe ? ' me' : '')}>
             <div className={"message-card" + (isMe ? ' me' : '')}>
-                <div className="message-card--header">{`${ip}, ${sender}`}</div>
+                <div className="message-card--header"><strong>{`${ip}, ${sender}`}</strong></div>
                 <div className="message-card--body">
                     <div className="message-card--body--text">{content}</div>
                     <div className="message-card--body--ts">{date}</div>
