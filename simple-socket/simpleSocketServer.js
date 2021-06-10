9428b7d18922c232ca803dd0286d6e7e00f5ed1a
cannaAriel <cannaariel@carmel6000.amitnet.org>
2021-06-10 09:04:00 +0300

    simple socket example

---
+++ simple-socket/simpleSocketServer.js
@@ -0,0 +1,26 @@
+const http = require("http")
+const socketio = require("socket.io")
+
+const server = http.createServer();
+
+const io = socketio(server, () => {
+    console.log(server);
+});
+
+io.on("connection", async (socket) => {
+    console.log('user connected to socket: ' + socket.id);
+
+    socket.on('join', () => {
+        console.log('join userId: ', socket.id);
+        socket.join(socket.id);
+        io.to(socket.id).emit('private', `hi, ${socket.id}`)
+    })
+
+    socket.on('message', ({ data }) => {
+        console.log('data: ', data, socket.id);
+        io.emit('response', `to everyone`)
+    })
+});
+
+
+server.listen(8080);
